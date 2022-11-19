797d6afcc0cca6fd602ee4da0583f715bdc6cdf2
amrlftr <amirulfitri143@gmail.com>
2022-11-19 23:12:46 +0800

    chore: update dependencies and config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Belleza', ...defaultTheme.fontFamily.serif],
+        sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
